7dced40c4fde12e442352c0374a976e5b45078df
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-01-04 03:37:43 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,8 +123,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
