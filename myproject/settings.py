00522c2e725dc6971a85eb1c75bffe54c6bc3d81
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-01-04 03:00:12 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,7 +119,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
